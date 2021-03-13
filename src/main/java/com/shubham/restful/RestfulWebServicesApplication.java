ba54b861626b8ba98f8fb4a72bdaec34d5ab69a9
Shubham Kumar <shubham908@gmail.com>
2021-03-13 13:17:25 +0530

    added code for i18n

--- src/main/java/com/shubham/restful/RestfulWebServicesApplication.java
+++ src/main/java/com/shubham/restful/RestfulWebServicesApplication.java
@@ -2,11 +2,33 @@ package com.shubham.restful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(RestfulWebServicesApplication.class, args);
+  }
+
+  @Bean
+  public LocaleResolver localeResolver() {
+    SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+    sessionLocaleResolver.setDefaultLocale(Locale.US);
+
+    return sessionLocaleResolver;
+  }
+
+  @Bean
+  public ResourceBundleMessageSource messageSource() {
+    ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+    resourceBundleMessageSource.setBasename("messages");
+
+    return resourceBundleMessageSource;
   }
 }
--- src/main/java/com/shubham/restful/controller/HelloController.java
+++ src/main/java/com/shubham/restful/controller/HelloController.java
@@ -1,14 +1,32 @@
 package com.shubham.restful.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloController {
+
+  private MessageSource messageSource;
+
+  @Autowired
+  public HelloController(MessageSource messageSource) {
+    this.messageSource = messageSource;
+  }
 
   @RequestMapping(value = "/hello-world", method = RequestMethod.GET)
   public String helloWorld() {
     return "Hello World";
+  }
+
+  @RequestMapping(value = "/hello-world-internationalized", method = RequestMethod.GET)
+  public String helloWorldInternationalized(
+      @RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+    return messageSource.getMessage("good.morning.message", null, locale);
   }
 }
