9edbd91f968901909f707d8d756adc318847dcbb
Shubham Kumar <shubham908@gmail.com>
2021-03-14 13:34:12 +0530

    added support for Swagger

---
+++ src/main/java/com/shubham/restful/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.shubham.restful.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final Contact DEFAULT_CONTACT =
+            new Contact("Shubham Kumar", "shubham.com", "shubham@shubham.com");
+
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
+            "API Title",
+            "API description", "1.0",
+            "urn:tos", DEFAULT_CONTACT,
+            "Apache 2.0",
+            "http://www.apache.org/licenses/LICENSE-2.0",
+            Arrays.asList());
+
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES =
+            new HashSet<>(Arrays.asList("application/json", "application/xml"));
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+    }
+}
--- src/main/java/com/shubham/restful/model/User.java
+++ src/main/java/com/shubham/restful/model/User.java
@@ -1,54 +1,61 @@
 package com.shubham.restful.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "Details about the user")
 public class User {
 
-  private Integer id;
+    private Integer id;
 
-  @Size(min = 2, message = "Name should have at least 2 characters")
-  private String name;
+    @Size(min = 2, message = "Name should have at least 2 characters")
+    @ApiModelProperty(notes = "Name should have at least 2 characters")
+    private String name;
 
-  @Past(message = "Birthdate cannot be a future date")
-  private Date birthDate;
+    @Past(message = "Birthdate cannot be a future date")
+    @ApiModelProperty(notes = "Birth date should be in the past")
+    private Date birthDate;
 
-  public User() {}
+    public User() {
+    }
 
-  public User(Integer id, String name, Date birthDate) {
-    super();
-    this.id = id;
-    this.name = name;
-    this.birthDate = birthDate;
-  }
+    public User(Integer id, String name, Date birthDate) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
 
-  public Integer getId() {
-    return id;
-  }
+    public Integer getId() {
+        return id;
+    }
 
-  public void setId(Integer id) {
-    this.id = id;
-  }
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  public Date getBirthDate() {
-    return birthDate;
-  }
+    public Date getBirthDate() {
+        return birthDate;
+    }
 
-  public void setBirthDate(Date birthDate) {
-    this.birthDate = birthDate;
-  }
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
 
-  @Override
-  public String toString() {
-    return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
-  }
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
+    }
 }
